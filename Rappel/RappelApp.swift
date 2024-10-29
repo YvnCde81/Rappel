48a211c3b8b29e2fbaade2cf3edae3500156c157
Yvan Gauthier <ygauthier81@icloud.com>
2024-10-29 16:51:01 +0100

    Initial Commit

---
+++ Rappel/RappelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RappelApp.swift
+//  Rappel
+//
+//  Created by Yvan Gauthier on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct RappelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RappelTests/RappelTests.swift
@@ -0,0 +1,17 @@
+//
+//  RappelTests.swift
+//  RappelTests
+//
+//  Created by Yvan Gauthier on 29/10/2024.
+//
+
+import Testing
+@testable import Rappel
+
+struct RappelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
